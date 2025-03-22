9d6ffe1a999107826505955d6b91a47ef0354f96
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-22 23:10:20 +0500

    chore: add bold , itlaic and underline functionality

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import React from "react";
-import { LucideIcon, Undo2Icon, Redo2Icon } from "lucide-react";
+import {
+  LucideIcon,
+  Undo2Icon,
+  Redo2Icon,
+  PrinterIcon,
+  SpellCheck,
+  BoldIcon,
+  ItalicIcon,
+  UnderlineIcon,
+} from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
+import { Separator } from "@/components/ui/separator";
 
 type ToolbarButtonProps = {
   onClick: () => void;
@@ -20,8 +30,8 @@ const ToolbarButton = ({
     <button
       onClick={onClick}
       className={cn(
-        "text-sm h-7 min-w-7 flex  items-center justify-center rounded-sm hover:bg-neutrl-200/80",
-        isActive && "bg-neutrl-200/80"
+        "text-sm h-7 min-w-7 flex  items-center justify-center rounded-sm hover:bg-neutral-200/80",
+        isActive && "bg-neutral-200/80"
       )}
     >
       <Icon className="size-4" />
@@ -49,11 +59,60 @@ export default function Toolbar() {
         icon: Redo2Icon,
         onClick: () => editor?.chain().focus().redo().run(),
       },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: "Spell Check",
+        icon: SpellCheck,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current === "false" ? "true" : "false"
+          );
+        },
+      },
+    ],
+    [
+      {
+        label: "Bold",
+        icon: BoldIcon,
+        onClick: () => {
+          editor?.chain().focus().toggleBold().run();
+        },
+      },
+      {
+        label: "Italic",
+        icon: ItalicIcon,
+        onClick: () => {
+          editor?.chain().focus().toggleItalic().run();
+        },
+      },
+      {
+        label: "Underline",
+        icon: UnderlineIcon,
+        onClick: () => {
+          editor?.chain().focus().toggleUnderline().run();
+        },
+      },
     ],
   ];
   return (
     <div className="bg-[#f1f4f9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
       {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/* <FontFamilyButton /> */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/* TODO: Heading */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/* TODO: Font Size */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {sections[1].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
     </div>
