cbbe96e3a0e3807beab0589c882c1c1f341034e1
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-24 19:35:49 +0500

    add: marker in ruler

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { FaCaretDown } from "react-icons/fa";
 const marker = Array.from({ length: 83 }, (_, i) => i);
 
 export default function Ruler() {
@@ -9,6 +9,13 @@ export default function Ruler() {
         id="ruler-container"
         className="max-w-[816px] mx-auto w-full h-full relative"
       >
+        <Marker
+          position={56}
+          isLeft={true}
+          isDragging={false}
+          onDoubleClick={() => {}}
+          onMouseDown={() => {}}
+        />
         <div className="absolute inset-0 h-full bottom-0 ">
           <div className=" relative w-[816px] h-full">
             {marker.map((marker) => {
@@ -21,13 +28,19 @@ export default function Ruler() {
                 >
                   {marker % 10 === 0 && (
                     <>
-                      <div className="w-[1px] h-2 bg-neutral-500 absolute bottom-0">
+                      <div className="w-[1px] h-2.5 bg-neutral-500 absolute bottom-0">
                         {" "}
                       </div>
                       <span className="bottom-2 text-[10px] absolute text-neutral-500 transform -translate-x-1/2 ">
                         {marker / 10 + 1}
                       </span>
                     </>
+                  )}
+                  {marker % 5 === 0 && marker % 10 !== 0 && (
+                    <div className="w-[1px] h-2 bg-neutral-500 absolute bottom-0"></div>
+                  )}
+                  {marker % 5 !== 0 && (
+                    <div className="w-[1px] h-1.5 bg-neutral-500 absolute bottom-0"></div>
                   )}
                 </div>
               );
@@ -38,3 +51,30 @@ export default function Ruler() {
     </div>
   );
 }
+
+type MarkerProps = {
+  position: number;
+  isLeft: boolean;
+  isDragging: boolean;
+  onMouseDown: () => void;
+  onDoubleClick: () => void;
+};
+
+const Marker = ({
+  position,
+  isDragging,
+  isLeft,
+  onDoubleClick,
+  onMouseDown,
+}: MarkerProps) => {
+  return (
+    <div
+      className="absolute top-0 w-4 h-full cursor-ew-resize z-[5] group -ml-2"
+      style={{ [isLeft ? "left" : "right"]: `${position}px` }}
+      onMouseDown={onMouseDown}
+      onDoubleClick={onDoubleClick}
+    >
+      <FaCaretDown className=" absolute left1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2 " />
+    </div>
+  );
+};
