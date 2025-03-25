81a098ec47f9f562309c722d18f1791bd35edbd2
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-25 23:11:59 +0500

    chore: add search input in home navbar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "./navbar";
 export default function page() {
   return (
     <div className=" min-h-screen flex flex-col">
-      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
         <Navbar />
       </div>
       <div className="mt-16">
