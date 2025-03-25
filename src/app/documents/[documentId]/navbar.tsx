43c4af9ddb768a7b110e8d4b04b1eb407b60f30e
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-25 12:50:59 +0500

    chore: in menu bar add file in file (print, remove , rename, new document save(json, html, pdf, text))

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -1,7 +1,32 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-
+import { BsFilePdf } from "react-icons/bs";
+import {
+  FileIcon,
+  FileJsonIcon,
+  GlobeIcon,
+  FileTextIcon,
+  FilePlusIcon,
+  FilePenIcon,
+  TrashIcon,
+  PrinterIcon,
+} from "lucide-react";
+import {
+  Menubar,
+  MenubarItem,
+  MenubarContent,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 import React from "react";
+import DocumentInput from "./document-input";
 
 export default function Navbar() {
   return (
@@ -11,8 +36,77 @@ export default function Navbar() {
           <Image src="/logo.svg" alt="Logo" width={36} height={36}></Image>
         </Link>
         <div className="flex flex-col">
-          {/* Document Input */}
+          <DocumentInput /> {/* Document Input */}
           {/* MenuBar */}
+          <div className="flex">
+            <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  File
+                </MenubarTrigger>
+                <MenubarContent className="print:hidden">
+                  <MenubarSub>
+                    <MenubarSubTrigger>
+                      <FileIcon className="size-4 mr-2" />
+                      Save
+                    </MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>
+                        <FileJsonIcon className="size-4 mr-2" />
+                        Json
+                      </MenubarItem>
+                      <MenubarItem>
+                        <GlobeIcon className="size-4 mr-2" />
+                        HTML
+                      </MenubarItem>
+                      <MenubarItem>
+                        <BsFilePdf className="size-4 mr-2" />
+                        PDF
+                      </MenubarItem>
+                      <MenubarItem>
+                        <FileTextIcon className="size-4 mr-2" />
+                        Text
+                      </MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                  <MenubarItem>
+                    <FilePlusIcon className="size-4 mr-2" />
+                    New Document
+                  </MenubarItem>
+                  <MenubarSeparator></MenubarSeparator>
+                  <MenubarItem>
+                    <FilePenIcon className="size-4 mr-2" />
+                    Reneme
+                  </MenubarItem>
+                  <MenubarItem>
+                    <TrashIcon className="size-4 mr-2" />
+                    Remove
+                  </MenubarItem>
+                  <MenubarSeparator></MenubarSeparator>
+                  <MenubarItem onClick={() => window.print()}>
+                    <PrinterIcon className="size-4 mr-2" />
+                    Print
+                    <MenubarShortcut>⌘p</MenubarShortcut>
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  Edit
+                </MenubarTrigger>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  Insert
+                </MenubarTrigger>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  Format
+                </MenubarTrigger>
+              </MenubarMenu>
+            </Menubar>
+          </div>
         </div>
       </div>
     </nav>
