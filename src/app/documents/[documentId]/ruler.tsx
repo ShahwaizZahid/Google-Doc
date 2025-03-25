22e6c52a562e71d16ba4126c335a303fd4c43edb
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-25 11:39:05 +0500

    fix: add ruler lines that show content management

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,3 +1,4 @@
+import { Scale } from "lucide-react";
 import React, { useRef, useState } from "react";
 import { FaCaretDown } from "react-icons/fa";
 const marker = Array.from({ length: 83 }, (_, i) => i);
@@ -144,6 +145,16 @@ const Marker = ({
       onDoubleClick={onDoubleClick}
     >
       <FaCaretDown className=" absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2 " />
+      <div
+        className="absolute left-1/2 top-4 transform -translate-x-1/2"
+        style={{
+          height: "100vh",
+          width: "1px",
+          transform: "scaleX(0.5)",
+          background: "#3b72f6",
+          display: isDragging ? "block" : "none",
+        }}
+      ></div>
     </div>
   );
 };
