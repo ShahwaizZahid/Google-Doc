13d44c663281f0a5be7ba2c9ddeed43b7b228399
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-25 22:26:39 +0500

    chore: add navbar on home page

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-import React from "react";
-export default function Home() {
-  return (
-    <div className="bg-red-800">
-      click <Link href={"documents/123"}>go to id page</Link>
-    </div>
-  );
-}
