5ee6f0d0d1afbc980e19ce6f60d1e346cb47019b
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-27 21:45:32 +0500

    create: post req to create document

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,4 +1,26 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+
+export const create = mutation({
+  args: {
+    title: v.optional(v.string()),
+    initialContent: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const documentId = await ctx.db.insert("documents", {
+      title: args.title ?? "Untitled document",
+      initialContent: args.initialContent,
+      ownerId: user.subject,
+    });
+    return documentId;
+  },
+});
 
 export const get = query({
   handler: async (ctx) => {
