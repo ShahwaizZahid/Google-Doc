cf11fd0978e5c0deff0eafacb1106c96166a13ce
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-27 13:06:08 +0500

    chore: constants of templates

---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: "blank",
+    label: "Blank Document",
+    imageUrl: "/template-images/blank-document.svg",
+  },
+  {
+    id: "software-proposal",
+    label: "Software development proposal",
+    imageUrl: "/template-images/software-proposal.svg",
+  },
+  {
+    id: "project-proposal",
+    label: "Project proposal",
+    imageUrl: "/template-images/project-proposal.svg",
+  },
+  {
+    id: "business-letter",
+    label: "Business letter",
+    imageUrl: "/template-images/business-letter.svg",
+  },
+  {
+    id: "resume",
+    label: "Resume",
+    imageUrl: "/template-images/resume.svg",
+  },
+  {
+    id: "cover-letter",
+    label: "Cover letter",
+    imageUrl: "/template-images/cover-letter.svg",
+  },
+  {
+    id: "letter",
+    label: "Letter",
+    imageUrl: "/template-images/letter.svg",
+  },
+];
