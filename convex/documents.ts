7f8ddd172ef332ae03ae16d129e92ec830c215d4
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-27 14:15:19 +0500

    chore: code the document schema

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,5 @@ import { query } from "./_generated/server";
 export const get = query({
   handler: async (ctx) => {
     return await ctx.db.query("documents").collect();
-    // do something with `tasks`
   },
 });
