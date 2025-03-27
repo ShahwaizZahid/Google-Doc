9c85921a3d380487a7f71be979bb87a1b91d4bf4
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-27 14:15:43 +0500

    chore: code the document schema

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import React from "react";
 import Navbar from "./navbar";
 import TemplatesGallery from "./templates-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+export default function Home() {
+  const documents = useQuery(api.documents.get);
 
-export default function page() {
+  if (documents === undefined) {
+    return <p>Loading...</p>;
+  }
   return (
     <div className=" min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -10,6 +18,9 @@ export default function page() {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
+        {documents?.map((document) => {
+          return <span key={document._id}>{document.title}</span>;
+        })}
       </div>
     </div>
   );
