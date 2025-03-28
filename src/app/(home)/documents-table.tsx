3764e2c091aaef79ba1ecc30d49bc10d7d6c04cb
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-28 21:55:29 +0500

    add: paginantion on recent documents

--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/table";
 import { LoaderIcon } from "lucide-react";
 import DocumentRow from "./document-row";
+import { Button } from "@/components/ui/button";
 
 type DocumentsTablesProps = {
   documents: Doc<"documents">[] | undefined;
@@ -59,6 +60,16 @@ export default function DocumentsTables({
           )}
         </Table>
       )}
+      <div className="flex items-center justify-center">
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => loadMore(5)}
+          disabled={status !== "CanLoadMore"}
+        >
+          {status === "CanLoadMore" ? "LoadMore" : "End of results"}
+        </Button>
+      </div>
     </div>
   );
 }
