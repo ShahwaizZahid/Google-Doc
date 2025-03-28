1294a2b70a71874342af35e574bfcd624bde3943
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-03-28 13:27:38 +0500

    chore: add custom remove dialge components

---
+++ src/components/remove-dialog.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogCancel,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Id } from "../../convex/_generated/dataModel";
+
+type RemoveDialogProps = {
+  documentId: Id<"documents">;
+  children: React.ReactNode; // Corrected the prop name
+};
+
+export default function RemoveDialog({
+  documentId,
+  children, // Corrected the prop name
+}: RemoveDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent onClick={(e) => e.stopPropagation()}>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            document.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={(e) => e.stopPropagation()}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
