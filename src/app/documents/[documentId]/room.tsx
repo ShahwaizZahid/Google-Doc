5647954c71882bec30a8b9c10dbef73c036e4870
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-04-05 21:43:43 +0500

    fix: show user data on comment and add tag functionality

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 import {
   LiveblocksProvider,
   RoomProvider,
@@ -8,12 +8,58 @@ import {
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
 import FullscreenLoader from "@/components/fullscreen-loader";
+import getUsers from "./action";
+import { toast } from "sonner";
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
 
 export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
 
+  const [users, setUsers] = useState<User[]>([]);
+
+  const fetchUsers = useMemo(
+    () => async () => {
+      try {
+        const list = await getUsers();
+        setUsers(list);
+      } catch {
+        toast.error("Failed to fetch users");
+      }
+    },
+    []
+  );
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
-    <LiveblocksProvider throttle={16} authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      throttle={16}
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={({ userIds }) => {
+        return userIds.map((userId) => {
+          return users.find((user) => user.id === userId) ?? undefined;
+        });
+      }}
+      resolveMentionSuggestions={({ text }) => {
+        let filterUsers = users; // Use let instead of const
+
+        if (text) {
+          filterUsers = users.filter((user) =>
+            user.name.toLowerCase().includes(text.toLowerCase())
+          );
+        }
+
+        return filterUsers.map((user) => user.id);
+      }}
+      resolveRoomsInfo={() => []}
+    >
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense
           fallback={<FullscreenLoader label="Room loading...." />}
