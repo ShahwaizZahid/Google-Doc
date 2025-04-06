a16e53bd2ba462a7c1fb4da0417b4d5239f15836
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-04-06 20:56:07 +0500

    refactor: code of document-input

--- src/app/documents/[documentId]/document-input.tsx
+++ src/app/documents/[documentId]/document-input.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { BsCloudCheck } from "react-icons/bs";
-export default function DocumentInput() {
+import { Id } from "../../../../convex/_generated/dataModel";
+
+type DocumentInputProps = {
+  title: string;
+  id: Id<"documents">;
+};
+export default function DocumentInput({ title }: DocumentInputProps) {
   return (
     <div className="flex items-center gap-2">
-      <span className="text-lg px-1.5 cursor-pointer truncate">
-        Untitled Document
-      </span>
+      <span className="text-lg px-1.5 cursor-pointer truncate">{title}</span>
       <BsCloudCheck />
     </div>
   );
