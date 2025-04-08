761e77e611c416140a1806d3a7f35ce56eb3ac6e
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-04-08 20:23:44 +0500

    create: tooltip component

---
+++ src/components/tooltip.tsx
@@ -0,0 +1,17 @@
+export default function Tooltip({
+  label,
+  children,
+}: {
+  label: string;
+  children: React.ReactNode;
+}) {
+  console.log(label);
+  return (
+    <div className="group relative inline-block">
+      <div className="opacity-0 group-hover:opacity-100 absolute top-full py-1  px-2 text-white text-xs rounded-lg mt-2.5 z-50 bg-black whitespace-nowrap transition-opacity">
+        {label}
+      </div>
+      {children}
+    </div>
+  );
+}
